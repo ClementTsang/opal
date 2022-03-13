46f7f0f1239adc5407048c313d495dad96368c84
ClementTsang <cjhtsang@uwaterloo.ca>
2022-03-13 18:58:17 -0400

    troubleshooting

--- scripts/optimize_sql.sql
+++ scripts/optimize_sql.sql
@@ -1,7 +1,8 @@
 pragma journal_mode = delete;
 pragma page_size = 1024;
+DELETE FROM english WHERE ROWID NOT IN (SELECT MIN(ROWID) FROM english GROUP BY word, phonemes);
 DROP INDEX IF EXISTS word_index;
-DROP INDEX IF EXISTS phoneme_index;
+DROP INDEX IF EXISTS phonemes_index;
 CREATE INDEX word_index ON english(word);
 CREATE INDEX phonemes_index ON english(phonemes);
 vacuum;
