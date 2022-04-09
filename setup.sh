c10daa46708a76bb1ada3d44909d439a858b4fbf
ClementTsang <cjhtsang@uwaterloo.ca>
2022-04-09 06:30:15 -0400

    copy license as well

--- setup.sh
+++ setup.sh
@@ -9,3 +9,4 @@ touch ./static/databases/db.sqlite3
 python3 ./scripts/cmudict/cmudict.py ./sources/cmudict ./static/databases/db.sqlite3
 sqlite3 static/databases/db.sqlite3 < scripts/optimize_sql.sql
 cp ./sources/cmudict/README ./static/databases/
+cp ./sources/cmudict/LICENSE ./static/databases/
