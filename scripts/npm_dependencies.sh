d998cc13b5431d01a5e4aeace78edb5531636924
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-03-08 01:21:22 -0500

    test cache

--- scripts/npm_dependencies.sh
+++ scripts/npm_dependencies.sh
@@ -2,11 +2,6 @@
 
 set -eux
 
-echo "Installing tailwindcss..."
 npm install -g tailwindcss@3.2.4
-
-echo "Installing binaryen..."
 npm install -g binaryen@111.0.0;
-
-echo "Installing minify..."
 npm install -g minify@9.2.0;
