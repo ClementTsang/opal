f4af169db9cb34c843a504dbd839bfa3418c50da
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-03-08 01:20:36 -0500

    fix cache flag

--- scripts/npm_dependencies.sh
+++ scripts/npm_dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -eux
+
+echo "Installing tailwindcss..."
 npm install -g tailwindcss@3.2.4
+
+echo "Installing binaryen..."
 npm install -g binaryen@111.0.0;
+
+echo "Installing minify..."
 npm install -g minify@9.2.0;
