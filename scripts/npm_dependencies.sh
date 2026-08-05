3b5ee2f34eda5d065bf84e2cf3e9fa340327bfac
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2026-08-05 00:44:20 -0400

    disable npm install scripts manually in script

--- scripts/npm_dependencies.sh
+++ scripts/npm_dependencies.sh
@@ -2,6 +2,6 @@
 
 set -eux
 
-npm install -g tailwindcss@3.2.4
-npm install -g binaryen@111.0.0;
-npm install -g minify@9.2.0;
+npm install -g tailwindcss@3.2.4 --ignore-scripts
+npm install -g binaryen@111.0.0 --ignore-scripts
+npm install -g minify@9.2.0 --ignore-scripts
